4f115ccbfe251759f5a9bd195e624be512e2e708
Babken <babkenmkhitaryan@gmail.com>
2017-07-02 12:17:55 +0400

    Added functionality to get current location.

--- UserProject/app/src/main/java/com/example/student/userproject/MapFragment.java
+++ UserProject/app/src/main/java/com/example/student/userproject/MapFragment.java
@@ -1,13 +1,22 @@
 package com.example.student.userproject;
 
 
+import android.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.arsy.maps_library.MapRipple;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,6 +31,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapRipple mapRipple;
+    private BroadcastReceiver mBroadcastReceiver;
+    private double currentLat;
+    private double currentLng;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -32,40 +44,106 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+            }
+            return;
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootV= inflater.inflate(R.layout.fragment_map, container, false);
+        Intent i = new Intent(getActivity(), LocationService.class);
+        getActivity().startService(i);
+        return rootV;
+    }
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    @Override
+     public void onResume() {
+        super.onResume();
+        if(mBroadcastReceiver == null) {
+            mBroadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    currentLat = (double) intent.getExtras().get("lat");
+                    currentLng = (double) intent.getExtras().get("lng");
+                    if (mMap != null) {
+                        mMap.clear();
+                        LatLng currentPosition = new LatLng(currentLat, currentLng);
+                        mMap.addMarker(new MarkerOptions().position(currentPosition).title("Marker in Sydney"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+                        mapRipple = new MapRipple(mMap, currentPosition, getActivity());
+                        mapRipple.withNumberOfRipples(3);
+                        mapRipple.withFillColor(Color.BLUE);
+                        mapRipple.withStrokeColor(Color.BLACK);
+                        mapRipple.withStrokewidth(10);      // 10dp
+                        mapRipple.withDistance(2000);      // 2000 metres radius
+                        mapRipple.withRippleDuration(12000);    //12000ms
+                        mapRipple.withTransparency(0.5f);
+                        mapRipple.startRippleMapAnimation();
+                    }
+                }
+            };
+        }
+        getActivity().registerReceiver(mBroadcastReceiver, new IntentFilter("LOCATION_UPDATE"));
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         SupportMapFragment fragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
         fragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         // TODO use d = sqrt((x2-x1)*(x2-x1) + (y2 -y1)(y2-y1)) formula to get users from nearly
         // TODO R sharavixy = map-i radari  withDistance-i argument
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mapRipple = new MapRipple(mMap, sydney, getActivity());
-        mapRipple.withNumberOfRipples(3);
-        mapRipple.withFillColor(Color.BLUE);
-        mapRipple.withStrokeColor(Color.BLACK);
-        mapRipple.withStrokewidth(10);      // 10dp
-        mapRipple.withDistance(2000);      // 2000 metres radius
-        mapRipple.withRippleDuration(12000);    //12000ms
-        mapRipple.withTransparency(0.5f);
-        mapRipple.startRippleMapAnimation();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mBroadcastReceiver != null) {
+            getActivity().unregisterReceiver(mBroadcastReceiver);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 10: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Toast.makeText(getActivity(), "Permissions is enabled", Toast.LENGTH_LONG).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
