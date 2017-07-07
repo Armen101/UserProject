94f05afcbee235948ad99d8087fdafab902eacb8
Armen Gevorgyan <gevorgyanweb@gmail.com>
2017-07-07 23:25:58 +0400

    adding SplashScreenActivity

---
+++ UserProject/app/src/main/java/com/example/student/userproject/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.example.student.userproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        ImageView splashScreen = (ImageView) findViewById(R.id.splash_screen);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation);
+        splashScreen.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                goToActivity();
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+    }
+
+    private void goToActivity() {
+        Intent intent= new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
